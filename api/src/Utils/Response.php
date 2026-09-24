03dfd37741618638b78337f049f9ebe6754b7422
alejosv07 <balrking07@gmail.com>
2026-09-23 18:54:37 -0600

    trying force cache

--- api/src/Utils/Response.php
+++ api/src/Utils/Response.php
@@ -1,17 +1,26 @@
 <?php
+
 declare(strict_types=1);
 
 namespace App\Utils;
 
-class Response {
+class Response
+{
     /**
      * 
      * @param mixed $data 
      * @param int $code
      */
-    public static function json(mixed $data, int $code = 200): void {
+    public static function json(mixed $data, int $code = 200): void
+    {
         http_response_code($code);
+
         header('Content-Type: application/json; charset=UTF-8');
+
+        header('Cache-Control: no-store, no-cache, must-revalidate, max-age=0');
+        header('Cache-Control: post-check=0, pre-check=0', false);
+        header('Pragma: no-cache');
+        header('Expires: 0');
 
         echo json_encode([
             'status' => ($code >= 200 && $code < 300) ? 'success' : 'error',
