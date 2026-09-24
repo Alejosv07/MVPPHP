916fbf8f8000db2930686acd28830eda1d6f53fc
alejosv07 <balrking07@gmail.com>
2026-09-23 18:15:13 -0600

    changing some error

--- api/public/index.php
+++ api/public/index.php
@@ -29,7 +29,7 @@ spl_autoload_register(function (string $class): void {
     }
 });
 
-require_once __DIR__ . '/../config/Database.php';
+require_once __DIR__ . '/../config/database.php';
 
 use App\Utils\Response;
 use App\Controllers\AuthController;
