fc2a0c625cf95b81712308f8f0350ff8352b7ff2
alejosv07 <balrking07@gmail.com>
2026-09-23 18:44:39 -0600

    restored

--- api/src/Models/CategoryModel.php
+++ api/src/Models/CategoryModel.php
@@ -13,15 +13,9 @@ class CategoryModel {
         $this->db = (new Database())->getConnection();
     }
 
-public function getAll(): array {
-    return [
-        [
-            'id' => 999,
-            'name' => 'TEST CATEGORY',
-            'description' => 'TEST'
-        ]
-    ];
-}
+    public function getAll(): array {
+        return $this->db->query("SELECT * FROM service_categories ORDER BY name ASC")->fetchAll();
+    }
 
     public function getById(int $id): array|false {
         $stmt = $this->db->prepare("SELECT * FROM service_categories WHERE id = ?");
